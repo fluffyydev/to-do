4a577e40ea727247b6a2fa694210ff0d79b0a7ab
fluffy <121191983+fluffyydev@users.noreply.github.com>
2025-03-21 12:34:30 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ var maxToDoLength = 10;
 // Enter Element items
 const enterBox = document.getElementById("createTaskButton");
 const enterTask = document.getElementById("newTaskText");
+
+enterBox.oninput = function() {
+  
+  const newListElement = document.createElement('li');
+
+  toDoList.appendChild(newListElement);
+  
+}
