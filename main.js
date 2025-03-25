718962572bf2a68781cd6fc6af7684c5cdbc1bd2
fluffy <121191983+fluffyydev@users.noreply.github.com>
2025-03-24 22:46:54 -0400

    stuff still dont do anything

--- main.js
+++ main.js
@@ -2,14 +2,22 @@ var toDoList = document.getElementById("toDoList");
 const enterElement = document.getElementById("enterItemElement");
 var maxToDoLength = 10;
 
-// Enter Element items
 const enterBox = document.getElementById("createTaskButton");
-const enterTask = document.getElementById("newTaskText");
+
 
 enterBox.oninput = function() {
-  
-  const newListElement = document.createElement('li');
+  const enterTask = document.getElementById("newTaskText");
+
+  const newListElement = document.createElement("li");
+  const newCheckBox = document.createElement("input");
+  newCheckBox.type = "checkbox";
+  newListElement.appendChild(newCheckBox);
+
+  const taskTextNode = document.createTextNode(" " + enterTask.value.trim());
+
+  newListElement.appendChild(newCheckBox);
+  newListElement.appendChild(taskTextNode);
 
   toDoList.appendChild(newListElement);
-  
+   
 }
